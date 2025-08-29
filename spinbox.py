f1712f6549505bf70b2af8082635f0849808b986
joselucas06 <joselucastorricogutierrez@gmail.com>
2025-08-29 07:35:15 -0400

    Update spinbox.py

--- spinbox.py
+++ spinbox.py
@@ -3,7 +3,7 @@ from tkinter import messagebox, ttk
  
 def mostrarEdad():
     tk.messagebox.showinfo("Edad", f"La edad seleccionada es: {spin.get()}")
- 
+    
 ventanaPrincipal=tk.Tk()
  
 labelEdad=tk.Label(ventanaPrincipal, text="Edad:")
@@ -12,5 +12,15 @@ spin=tk.Spinbox(ventanaPrincipal, from_=1, to=100)
 spin.grid(row=0, column=1, padx=10, pady=10)
 boton=tk.Button(ventanaPrincipal, text="Obtener valor", command=lambda:mostrarEdad())
 boton.grid(row=1, column=0, padx=10, pady=10)
+
+def mostrargenero():
+    tk.messagebox.showinfo("Genero",f"El genero seleccionado es:{genero.get()}")
+
+labelGenero=tk.Label(ventanaPrincipal, text="Genero")
+labelGenero.grid(row=2, column=0, padx=5, pady=5, sticky="w")
+genero=tk.Spinbox(ventanaPrincipal, values=("masculino","femenino","otro"))
+genero.grid(row=2,column=1,padx=10,pady=10)
+botonGenero=tk.Button(ventanaPrincipal,text="obtener genero",command=mostrargenero)
+botonGenero.grid(row=3,column=0,padx=10,pady=10)
  
 ventanaPrincipal.mainloop()
